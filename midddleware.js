d0ea0f28d012ff5e83cf6bb220da025e56af9cf5
Aditi Bobde <Aditib@WinjitTechnologiesPvtLtd.local>
2020-04-23 15:14:50 +0530

    Refactor middleware

---
+++ midddleware.js
@@ -0,0 +1,16 @@
+
+     var middleware = {
+        requireAuthentication: function(req, res, next){
+            console.log('private route hit!');
+            next();
+        },
+        logger: function(req, res, next){
+            var date = new Date().toString();
+            console.log('Request: '+ req.method + ' ' + req.originalUrl + ' ' + date);
+            next();
+        }
+    }
+
+    module.exports = {
+        middleware
+    }
--- server.js
+++ server.js
@@ -1,22 +1,11 @@
 var express = require('express');
 var app = express();
 var PORT = 3005;
-var middleware = {
-    requireAuthentication: function(req, res, next){
-        console.log('private route hit!');
-        next();
-    },
-    logger: function(req, res, next){
-        var date = new Date().toString();
-        console.log('Request: '+ req.method + ' ' + req.originalUrl + ' ' + date);
-        next();
-    }
-};
+var m = require('./midddleware');
+app.use(m.middleware.logger);
 
-app.use(middleware.logger);
-
-app.get('/about',middleware.requireAuthentication,function(req,res){
-    res.send('Called you Mine!')
+app.get('/about',m.middleware.requireAuthentication,function(req,res){
+    res.send('Express working!')
 });
 
 app.use(express.static(__dirname + '/public'));
